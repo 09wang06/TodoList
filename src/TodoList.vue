575055fcb9c18841e2f74c78a7eb6a44b2c2252e
09wang06 <1121164664@qq.com>
2019-12-29 15:07:07 +0800

    todolist

---
+++ src/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div >
+    <div>
+      <input v-model="inputValue"/>
+      <button @click="HandSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item, index ) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="HistroyDelete"
+      >
+        
+      </todo-item>
+      
+    </ul>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem'
+  export default {
+    components:{
+      'todo-item':TodoItem,
+    },
+    data () {   //相当于 data:function(){
+      return {
+        inputValue:'',
+        list: [],
+      }
+    },
+    methods:{
+      HandSubmit () {
+        this.list.push(this.inputValue);  //this.$emit.list.push()
+        this.inputValue='';
+      },
+      HistroyDelete(index){
+        this.list.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li @click="HistroyClick">
+    {{content}}
+  </li>
+</template>
+
+<script>
+  export default{
+    props:['content','index'],
+    methods:{
+      HistroyClick() {
+        alert("---第"+(this.index+1)+"条记录已被删除！--")
+        this.$emit('delete', this.index);
+      }
+    }
+  }
+
+</script>
+
+<!--样式表部分-->
+<style scoped>
+
+
+</style>
